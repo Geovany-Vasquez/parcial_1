02d7af5c4c2aba57224fbfc2996e87b7dd4aae23
GeovanyVasquez <gvasquezp5@miumg.edu.gt>
2021-03-10 17:56:46 -0600

    parcial

---
+++ parcial_1.cpp
@@ -0,0 +1,264 @@
+#include <iostream>
+#include <stdio.h>  
+#include <string.h> 
+
+
+using namespace std;
+const char *nombe_archivo = "archivo.dat";
+int bonificacion = 250;
+struct Empleado{
+	int codigo;
+	char nombre[50];
+	char apellido[50];
+	char  puesto[50];
+	char sueldo[10];
+	
+};
+ void ingresar_empleado();
+void abrir_empleado();
+ void modificar_empleado();
+ void eliminar();
+int main (){
+	abrir_empleado();
+ ingresar_empleado();
+	modificar_empleado();
+	eliminar();
+	return 0;	
+}
+void buscar_codigo(){
+	
+	FILE* archivo = fopen(nombe_archivo, "rb");
+	
+	int indice=0,pos=0,cod=0;
+	cout<<"BUSCAR CODIGO: ";
+	cin>>cod;
+	Empleado empleado;
+	
+	fread ( &empleado, sizeof(Empleado), 1, archivo );
+		
+		do{
+			
+			if (empleado.codigo == cod){
+			pos = indice;
+			}
+	
+		fread ( &empleado, sizeof(Empleado), 1, archivo );
+		indice += 1;
+		} while (feof( archivo ) == 0);
+		
+	cout<<"****************** "<<pos<<"******************"<<endl;
+	fseek ( archivo,pos * sizeof(Empleado), SEEK_SET );
+
+    fread ( &empleado, sizeof( Empleado ), 1, archivo );
+
+    	cout << "Codigo: " << empleado.codigo << endl;
+        cout << "Nombre: " << empleado.nombre << endl;
+        cout << "Apellido: " << empleado.apellido << endl;
+        cout << "Puesto: " << empleado.puesto << endl;
+        cout << "Sueldo: " << empleado.sueldo << endl;
+        cout << "bonificacion: " <<bonificacion << endl;
+       
+        
+        cout << endl;
+	
+	fclose(archivo);
+	system("PAUSE");
+}
+
+
+void buscar_indice(){
+	FILE* archivo = fopen(nombe_archivo, "rb");
+	
+	int pos=0;
+	cout<<"Que registro desea ver: ";
+	cin>>pos;
+	cout<<"****************** "<<pos<<" ******************"<<endl;
+	fseek ( archivo,pos * sizeof(Empleado), SEEK_SET );
+	
+	Empleado empleado;
+	
+    fread ( &empleado, sizeof( Empleado ), 1, archivo );
+
+    cout << "Codigo: " << empleado.codigo << endl;
+        cout << "Nombre: " << empleado.nombre << endl;
+        cout << "Apellido: " << empleado.apellido << endl;
+        cout << "Puesto: " << empleado.puesto << endl;
+        cout << "Sueldo: " << empleado.sueldo << endl;
+        cout << "Bonificacion: " <<bonificacion<< endl;
+	
+	fclose(archivo);
+
+	
+}
+
+
+void abrir_empleado(){
+
+	system("cls");
+	FILE* archivo = fopen(nombe_archivo, "rb");
+	if(!archivo) {
+		archivo = fopen(nombe_archivo, "w+b");
+	}
+	Empleado empleado;
+	int registro=0;
+	fread ( &empleado, sizeof(Empleado), 1, archivo );
+//	cout<<"_____________________________________________________________________________________________________________"<<endl;
+	cout << "id" <<"|"<< "CODIGO" <<"|"<< "NOMBRE"<<" | "<<"APELLIDO"<<" | "<<"PUESTO"<<" | "<<"SUELDO"<<" | "<<"BONIFICACION"<<" | "<<endl;	
+		do{
+		cout<<"____________________________________________________________________________________________________________________________"<<endl;
+		cout << registro <<" |  "<< empleado.codigo <<" | "<< empleado.nombre<<" | "<<empleado.apellido<<" | "<<empleado.puesto<<" | "<<empleado.sueldo<<" | "<<bonificacion<<endl;
+        
+        
+		fread ( &empleado, sizeof(Empleado), 1, archivo );
+		registro += 1;	
+		} while (feof( archivo ) == 0);																																																				
+		
+    cout<<endl;
+    
+		fclose(archivo);
+	}
+void ingresar_empleado(){
+	
+	char continuar;
+	FILE* archivo = fopen(nombe_archivo, "ab"); // ab
+	
+	
+		Empleado empleado;
+		 string nombre,apellido;
+		do{
+			fflush(stdin);
+			
+		cout<<"Ingrese el Codigo:";
+		cin>>empleado.codigo;
+        cin.ignore();
+        
+		cout<<"Ingrese el Nombre:";
+		getline(cin,nombre);
+    	strcpy(empleado.nombre, nombre.c_str()); 
+			
+		cout<<"Ingrese el Apellido:";
+		getline(cin,apellido);
+		strcpy(empleado.apellido, apellido.c_str()); 
+		
+		cout<<"Ingrese el Puesto:";
+		cin>>empleado.puesto;
+		cin.ignore();
+		
+	     cout<<"Ingrese el sueldo:";
+		cin>>empleado.sueldo;
+        cin.ignore();
+		 
+	
+		
+		fwrite( &empleado, sizeof(Empleado), 1, archivo );
+		
+		cout<<"Desea Agregar otro Nombre s/n : ";
+		cin>>continuar;
+		} while((continuar=='s') || (continuar=='S') );
+		
+	
+	fclose(archivo);
+	abrir_empleado();
+	buscar_indice();	
+	buscar_codigo();
+}
+void modificar_empleado(){
+	
+	FILE* archivo = fopen(nombe_archivo, "r+b");
+	
+		int id;
+		string nombre,apellido;	
+		char sueldo,bonificacion,puesto;
+    	Empleado empleado;
+    
+		cout << "Ingrese el ID que desea Modificar: ";
+    		cin >> id;
+    		fseek ( archivo, id * sizeof(Empleado), SEEK_SET );  // esto es lo que permite modificar en la pocision
+	
+		cout<<"Ingrese el Codigo:";
+		cin>>empleado.codigo;
+        cin.ignore();
+        
+		cout<<"Ingrese el Nombre:";
+		getline(cin,nombre);
+    	strcpy(empleado.nombre, nombre.c_str()); 
+			
+		cout<<"Ingrese el Apellido:";
+		getline(cin,apellido);
+		strcpy(empleado.apellido, apellido.c_str()); 
+		
+		
+		cout<<"Ingrese puesto:";
+		cin>>empleado.puesto;
+		cin.ignore();
+	
+		
+		cout<<"Ingrese sueldo:";
+		cin>>empleado.sueldo;
+		cin.ignore();
+		
+		
+		fwrite( &empleado, sizeof(Empleado), 1, archivo );
+		
+	fclose(archivo);
+	abrir_empleado();
+		system("PAUSE");
+}
+void eliminar(){
+      int pos = 0;
+      cout<<"\n Ingrese el id a eliminar: "<<endl;
+      cin>>pos;
+
+      FILE* archivo = fopen(nombe_archivo, "r+b");
+      FILE* archivo2 = fopen("auxis.dat", "a+b");
+      Empleado empleado;
+      fread ( &empleado, sizeof(Empleado), 1, archivo );
+
+      while (feof( archivo ) == 0){
+          if (pos == empleado.codigo){
+              cout<<"\n El registro ah sido eliminado correctamente "<<endl;
+              cout<<endl;
+        }else 
+           fwrite( &empleado, sizeof(Empleado), 1, archivo2 );
+        fread ( &empleado, sizeof(Empleado),1, archivo);
+
+      }
+      fclose(archivo);
+      remove("archivo.dat");
+      rename("auxi.dat","archivo.dat");
+      system("pause");
+
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+
+
